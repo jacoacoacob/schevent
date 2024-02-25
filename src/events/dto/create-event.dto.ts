51de55e37944765218244a7f5e7308a88faf093f
jacoacoacob <jacoacoacob@protonmail.com>
2024-02-25 15:54:38 -0600

    minimal ApiProperty decorator usage

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,6 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateEventDto {
+  @ApiProperty()
   name: string;
+
+  @ApiProperty()
   description: string;
+
+  @ApiProperty()
   timestamp: string;
+
+  @ApiProperty()
   invitees: string[];
 }
