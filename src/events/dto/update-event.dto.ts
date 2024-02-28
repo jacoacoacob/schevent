ea76897218929452ab073cbbc5ad5d2601d2b7eb
jacoacoacob <jacoacoacob@protonmail.com>
2024-02-27 18:55:10 -0600

    swagger ApiProperty workaround in UpdateEventDto

--- src/events/dto/update-event.dto.ts
+++ src/events/dto/update-event.dto.ts
@@ -1,4 +1,20 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEventDto } from './create-event.dto';
+import { ApiProperty } from '@nestjs/swagger';
+// import { PartialType } from '@nestjs/mapped-types';
+// import { CreateEventDto } from './create-event.dto';
 
-export class UpdateEventDto extends PartialType(CreateEventDto) {}
+// TODO: Get swagger to recogize `ApiField`s from parent Dto class
+export class UpdateEventDto {
+  @ApiProperty({ required: false })
+  name?: string;
+
+  @ApiProperty({ required: false })
+  description?: string;
+
+  @ApiProperty({ required: false })
+  timestamp?: string;
+
+  @ApiProperty({ required: false })
+  invitees?: string[];
+}
+
+// export class UpdateEventDto extends PartialType(CreateEventDto) {}
